b5c7cd9708b54593406c1cd500c4b4780bc4fb03
Ben Eppel <ben@beneppelsoftware.com>
2020-08-21 11:16:46 +0100

    DRTII-467 Update uPickle

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object Dependencies {
   val scalaTest = "3.0.8"
+  val uPickle = "1.2.0"
   val autowire = "0.2.6"
-  val uPickle = "0.6.7"
   val booPickle = "1.2.6"
   val specs2 = "4.10.0"
 
   val libDeps = Seq(
     "org.scalatest" %% "scalatest" % "3.0.8",
-    "com.lihaoyi" %% "autowire" % autowire,
     "com.lihaoyi" %% "upickle" % uPickle,
+    "com.lihaoyi" %% "autowire" % autowire,
     "io.suzaku" %% "boopickle" % booPickle,
     "org.specs2" %% "specs2-core" % specs2 % "test"
   )
