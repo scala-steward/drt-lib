a44e3ad44418e574954624b31a5d69e1505422d2
Rich Birch <ringo@albumsnaps.com>
2021-07-30 10:39:27 +0100

    DRTII-525 Update port codes test to include PIK

--- src/test/scala/uk/gov/homeoffice/drt/auth/RolesTest.scala
+++ src/test/scala/uk/gov/homeoffice/drt/auth/RolesTest.scala
@@ -2,12 +2,12 @@ package uk.gov.homeoffice.drt.auth
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import uk.gov.homeoffice.drt.auth.Roles.{BFS, BHD, BHX, BRS, EDI, EMA, GLA, LCY, LGW, LHR, LPL, LTN, MAN, NCL, STN}
+import uk.gov.homeoffice.drt.auth.Roles._
 
 class RolesTest extends AnyWordSpec with Matchers {
   "The port roles list" should {
     "Contain the correct ports" in {
-      Roles.portRoles should ===(Set(BFS, BHD, BHX, BRS, EDI, EMA, GLA, LCY, LGW, LHR, LPL, LTN, MAN, NCL, STN))
+      Roles.portRoles should ===(Set(BFS, BHD, BHX, BRS, EDI, EMA, GLA, LCY, LGW, LHR, LPL, LTN, MAN, NCL, PIK, STN))
     }
   }
 }
