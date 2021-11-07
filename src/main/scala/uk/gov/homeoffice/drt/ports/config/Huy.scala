851faa463b34e6be459cbee7759e4b16d51e0514
Rich Birch <ringo@albumsnaps.com>
2021-11-07 11:49:45 +0000

    DRTII-484 DSA & HUY config added

---
+++ src/main/scala/uk/gov/homeoffice/drt/ports/config/Huy.scala
@@ -0,0 +1,43 @@
+package uk.gov.homeoffice.drt.ports.config
+
+import uk.gov.homeoffice.drt.auth.Roles.{HUY, LBA}
+import uk.gov.homeoffice.drt.ports.PaxTypes._
+import uk.gov.homeoffice.drt.ports.Queues._
+import uk.gov.homeoffice.drt.ports.Terminals._
+import uk.gov.homeoffice.drt.ports._
+
+import scala.collection.immutable.SortedMap
+import scala.concurrent.duration.DurationInt
+
+object Huy extends AirportConfigLike {
+
+  import AirportConfigDefaults._
+
+  val config: AirportConfig = AirportConfig(
+    portCode = PortCode("HUY"),
+    queuesByTerminal = SortedMap(
+      T1 -> Seq(Queues.EeaDesk, Queues.NonEeaDesk)
+    ),
+    slaByQueue = Map(
+      Queues.EeaDesk -> 25,
+      Queues.NonEeaDesk -> 45
+    ),
+    defaultWalkTimeMillis = Map(T1 -> 5.minutes.toMillis),
+    terminalPaxSplits = List(T1).map(t => (t, defaultPaxSplits)).toMap,
+    terminalProcessingTimes = Map(T1 -> defaultProcessingTimes),
+    minMaxDesksByTerminalQueue24Hrs = Map(T1 -> Map(
+      Queues.EeaDesk -> (List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)),
+      Queues.NonEeaDesk -> (List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+    )),
+    eGateBankSizes = Map(),
+    role = HUY,
+    terminalPaxTypeQueueAllocation = Map(
+      T1 -> (defaultQueueRatios + (
+        EeaMachineReadable -> List(EeaDesk -> 1.0),
+        B5JPlusNational -> List(Queues.EeaDesk -> 1.0)
+      ))),
+    feedSources = Seq(ApiFeedSource, LiveBaseFeedSource, AclFeedSource),
+    flexedQueues = Set(EeaDesk, NonEeaDesk),
+    desksByTerminal = Map(T1 -> 3)
+  )
+}
