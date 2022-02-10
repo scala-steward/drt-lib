9f4cb1a66e0417709b04d8a244c8d74301454dbc
Rich Birch <ringo@albumsnaps.com>
2022-02-10 17:26:48 +0000

    DRTII-924 Create regions and assign ports

---
+++ src/main/scala/uk/gov/homeoffice/drt/ports/PortRegion.scala
@@ -0,0 +1,31 @@
+package uk.gov.homeoffice.drt.ports
+
+sealed trait PortRegion {
+  val name: String
+  val ports: Set[PortCode]
+}
+
+object PortRegion {
+  val regions = Set(North, South, Central, Heathrow)
+
+  object North extends PortRegion {
+    override val name: String = "North"
+    override val ports: Set[PortCode] =
+      Set("BFS", "BHD", "DSA", "EDI", "GLA", "HUY", "LBA", "LPL", "MAN", "MME", "NCL", "PIK").map(PortCode(_))
+  }
+
+  object Central extends PortRegion {
+    override val name: String = "Central"
+    override val ports: Set[PortCode] = Set("BHX", "EMA", "LCY", "LTN", "NYI", "STN").map(PortCode(_))
+  }
+
+  object South extends PortRegion {
+    override val name: String = "South"
+    override val ports: Set[PortCode] = Set("BOH", "BRS", "CWL", "EXT", "LGW", "NQY", "SOU").map(PortCode(_))
+  }
+
+  object Heathrow extends PortRegion {
+    override val name: String = "Heathrow"
+    override val ports: Set[PortCode] = Set(PortCode("LHR"))
+  }
+}
