8899427055cf8a76231792e94fe16d1d3c017f82
Rich Birch <ringo@albumsnaps.com>
2022-07-13 11:06:02 +0100

    DRTII-1047 Customise upickle serialiser for Splits

---
+++ shared/src/test/scala/uk/gov/homeoffice/drt/arrivals/SplitsTest.scala
@@ -0,0 +1,31 @@
+package uk.gov.homeoffice.drt.arrivals
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import uk.gov.homeoffice.drt.Nationality
+import uk.gov.homeoffice.drt.arrivals.EventTypes.DC
+import uk.gov.homeoffice.drt.arrivals.SplitStyle.PaxNumbers
+import uk.gov.homeoffice.drt.ports.PaxTypes.GBRNational
+import uk.gov.homeoffice.drt.ports.Queues.EeaDesk
+import uk.gov.homeoffice.drt.ports.SplitRatiosNs.SplitSources
+import uk.gov.homeoffice.drt.ports.{ApiPaxTypeAndQueueCount, PaxAge}
+import upickle.default.{read, writeJs}
+
+class SplitsTest extends AnyWordSpec with Matchers {
+  "Splits" should {
+    "serialise to json and back" in {
+      val splits = Splits(
+        Set(ApiPaxTypeAndQueueCount(GBRNational, EeaDesk, 1, Option(Map(Nationality("GBR") -> 1)), Option(Map(PaxAge(25) -> 1)))),
+        SplitSources.Historical,
+        Option(DC),
+        PaxNumbers
+      )
+      val serialised = writeJs(splits)
+      val deserialised = read[Splits](serialised)
+
+      println(s"deserialised: $deserialised")
+
+      deserialised shouldBe splits
+    }
+  }
+}
