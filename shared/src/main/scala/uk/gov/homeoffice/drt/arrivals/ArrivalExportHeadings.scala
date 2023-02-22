d66af5991b1d121f4eea1a85bf37b99749b1fd7d
Rich Birch <ringo@albumsnaps.com>
2023-02-22 10:54:14 +0000

    DRTII-1159 Simplify export headings

---
+++ shared/src/main/scala/uk/gov/homeoffice/drt/arrivals/ArrivalExportHeadings.scala
@@ -0,0 +1,48 @@
+package uk.gov.homeoffice.drt.arrivals
+
+import uk.gov.homeoffice.drt.ports.Queues.Queue
+import uk.gov.homeoffice.drt.ports.{PaxTypesAndQueues, Queues}
+import uk.gov.homeoffice.drt.splits.ApiSplitsToSplitRatio
+
+object ArrivalExportHeadings {
+
+  private val queueNamesInOrder: Seq[Queue] = ApiSplitsToSplitRatio.queuesFromPaxTypeAndQueue(PaxTypesAndQueues.inOrder)
+
+  private val arrivalHeadings: String = Seq(
+    "IATA",
+    "ICAO",
+    "Origin",
+    "Gate/Stand",
+    "Status",
+    "Scheduled",
+    "Predicted Arrival",
+    "Est Arrival",
+    "Act Arrival",
+    "Est Chox",
+    "Act Chox",
+    "Minutes off scheduled",
+    "Est PCP",
+    "Total Pax",
+    "PCP Pax",
+    "Invalid API"
+  ).mkString(",")
+
+  private val splitsHeadings: String = Seq("API", "Historical", "Terminal Average").map(headingsForSplitSource).mkString(",")
+
+  private val actualApiHeadings: String = PaxTypesAndQueues.inOrder.map(heading => s"API Actual - ${heading.displayName}").mkString(",")
+
+  private val apiAdditionalHeadings: String = Seq("Nationalities", "Ages").mkString(",")
+
+  private val regionalExportPrefixHeadings: String = Seq("Region", "Port", "Terminal").mkString(",")
+
+  val arrivalWithSplitsHeadings: String = Seq(arrivalHeadings, splitsHeadings).mkString(",")
+
+  val arrivalWithSplitsAndRawApiHeadings: String = Seq(arrivalWithSplitsHeadings, actualApiHeadings, apiAdditionalHeadings).mkString(",")
+
+  val regionalExportHeadings: String = Seq(regionalExportPrefixHeadings, arrivalWithSplitsHeadings).mkString(",")
+
+  private def headingsForSplitSource(source: String): String = queueNamesInOrder
+    .map(q => s"$source ${Queues.displayName(q)}")
+    .mkString(",")
+
+}
