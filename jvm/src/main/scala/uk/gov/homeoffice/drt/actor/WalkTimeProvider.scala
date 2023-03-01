ed1fe7f04d1d9041009bd246f6c759bf40269c00
Rich Birch <ringo@albumsnaps.com>
2023-03-01 11:24:24 +0000

    DRTII-1157 Move provider to production code

---
+++ jvm/src/main/scala/uk/gov/homeoffice/drt/actor/WalkTimeProvider.scala
@@ -0,0 +1,35 @@
+package uk.gov.homeoffice.drt.actor
+
+import org.apache.commons.csv.{CSVFormat, CSVParser}
+import uk.gov.homeoffice.drt.arrivals.Arrival
+import uk.gov.homeoffice.drt.ports.Terminals.Terminal
+
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import scala.util.Try
+
+case class WalkTimeProvider(walkTimes: Map[(Terminal, String), Int]) {
+  def walkTime(arrival: Arrival): Option[Int] = {
+    val terminal = arrival.Terminal
+    val standOrGate = arrival.Stand.getOrElse(arrival.Gate.getOrElse(""))
+    walkTimes.get((terminal, standOrGate))
+  }
+}
+
+object WalkTimeProvider {
+  def apply(csvPath: String): WalkTimeProvider = {
+    val source = scala.io.Source.fromFile(csvPath)
+    val csvContent = source.getLines().mkString("\n")
+    source.close()
+    val csv = CSVParser.parse(csvContent, CSVFormat.DEFAULT.withFirstRecordAsHeader())
+    val rows = csv.iterator().asScala.toList
+
+    val times: Map[(Terminal, String), Int] = rows.map { row =>
+      val gateOrStand = Try(row.get("gate")).getOrElse(row.get("stand"))
+      val terminal = Terminal(row.get("terminal"))
+      val walkTime = row.get("walktime").toInt
+      ((terminal, gateOrStand), walkTime)
+    }.toMap
+
+    WalkTimeProvider(times)
+  }
+}
