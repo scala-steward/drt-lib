af7976d874b70698174664b4134e61d8e08ae997
Rich Birch <ringo@albumsnaps.com>
2023-03-08 13:12:46 +0000

    DRTII-1157 Fix incorrect prefix

--- shared/src/main/scala/uk/gov/homeoffice/drt/prediction/arrival/ArrivalModelAndFeatures.scala
+++ shared/src/main/scala/uk/gov/homeoffice/drt/prediction/arrival/ArrivalModelAndFeatures.scala
@@ -5,8 +5,8 @@ import uk.gov.homeoffice.drt.prediction.ModelAndFeatures
 import uk.gov.homeoffice.drt.time.SDateLike
 
 trait ArrivalModelAndFeatures extends ModelAndFeatures {
-  def dayOfWeek(ts: Long)(implicit sDateProvider: Long => SDateLike): String = s"dow_${sDateProvider(ts).getDayOfWeek()}"
-  def amPm(ts: Long)(implicit sDateProvider: Long => SDateLike): String = s"dow_${sDateProvider(ts).getHours() / 12}"
+  private def dayOfWeek(ts: Long)(implicit sDateProvider: Long => SDateLike): String = s"dow_${sDateProvider(ts).getDayOfWeek()}"
+  private def amPm(ts: Long)(implicit sDateProvider: Long => SDateLike): String = s"pod_${sDateProvider(ts).getHours() / 12}"
 
   def prediction(arrival: Arrival)(implicit sDateProvider: Long => SDateLike): Option[Int] = {
     val dowIdx = features.oneToManyValues.indexOf(dayOfWeek(arrival.Scheduled))
