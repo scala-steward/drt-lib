1eac335dbbfddb8ee52153af0e265f303abd9a82
Rich Birch <rich@freshcodelimited.com>
2023-08-07 09:49:05 +0100

    DRTII-1206 Extract splits update function

--- shared/src/main/scala/uk/gov/homeoffice/drt/arrivals/SplitsForArrivals.scala
+++ shared/src/main/scala/uk/gov/homeoffice/drt/arrivals/SplitsForArrivals.scala
@@ -1,17 +1,43 @@
 package uk.gov.homeoffice.drt.arrivals
 
 import uk.gov.homeoffice.drt.DataUpdates.FlightUpdates
-import uk.gov.homeoffice.drt.ports.FeedSource
+import uk.gov.homeoffice.drt.arrivals.SplitsForArrivals.updateFlightWithSplits
+import uk.gov.homeoffice.drt.ports.SplitRatiosNs.SplitSources.ApiSplitsWithHistoricalEGateAndFTPercentages
+import uk.gov.homeoffice.drt.ports.{ApiFeedSource, FeedSource}
 
 
 object SplitsForArrivals {
   val empty: SplitsForArrivals = SplitsForArrivals(Map())
+
+  def updateSplits(existing: Set[Splits], incoming: Set[Splits]): Set[Splits] =
+    (existing.map(s => (s.source, s)).toMap ++ incoming.map(s => (s.source, s)).toMap).values.toSet
+
+  def updateFlightWithSplits(flightWithSplits: ApiFlightWithSplits, splits: Set[Splits], nowMillis: Long): ApiFlightWithSplits = {
+    val updatedArrival = splits.find(_.source == ApiSplitsWithHistoricalEGateAndFTPercentages) match {
+      case None =>
+        flightWithSplits.apiFlight
+      case Some(liveSplit) =>
+        val totalPax: Int = Math.round(liveSplit.totalPax)
+        val transPax: Int = Math.round(liveSplit.totalPax - liveSplit.totalExcludingTransferPax).toInt
+        val sources = flightWithSplits.apiFlight.FeedSources + ApiFeedSource
+        val totalPaxSources = flightWithSplits.apiFlight.PassengerSources.updated(ApiFeedSource, Passengers(Some(totalPax), Option(transPax)))
+        flightWithSplits.apiFlight.copy(
+          FeedSources = sources,
+          PassengerSources = totalPaxSources
+        )
+    }
+    val updatedSplits = updateSplits(flightWithSplits.splits, splits)
+    val updatedFlightWithSplits = flightWithSplits.copy(
+      apiFlight = updatedArrival,
+      splits = updatedSplits,
+      lastUpdated = Option(nowMillis),
+    )
+    updatedFlightWithSplits
+  }
+
 }
 
 case class SplitsForArrivals(splits: Map[UniqueArrival, Set[Splits]]) extends FlightUpdates {
-
-  private def updateSplits(existing: Set[Splits], incoming: Set[Splits]): Set[Splits] =
-    (existing.map(s => (s.source, s)).toMap ++ incoming.map(s => (s.source, s)).toMap).values.toSet
 
   def diff(other: Map[UniqueArrival, Set[Splits]]): SplitsForArrivals = {
     val updatedSplits = splits
@@ -39,17 +65,16 @@ case class SplitsForArrivals(splits: Map[UniqueArrival, Set[Splits]]) extends Fl
     }.toSet
     val updatedFlights = splits.foldLeft(flightsWithSplits.flights) {
       case (acc, (key, incoming)) =>
-        println(s"looking for $key to update splits. keys: ${acc.keys.map(_.toString).mkString(", ")}")
         acc.get(key) match {
           case Some(flightWithSplits) =>
-            val updatedSplits = updateSplits(flightWithSplits.splits, incoming)
-            val updatedFlightWithSplits = flightWithSplits.copy(splits = updatedSplits, lastUpdated = Option(nowMillis))
+            val updatedFlightWithSplits = updateFlightWithSplits(flightWithSplits, incoming, nowMillis)
             acc + (key -> updatedFlightWithSplits)
           case None => acc
         }
     }
     (FlightsWithSplits(updatedFlights), minutesFromUpdates)
   }
+
 
   def ++(tuple: (UniqueArrival, Set[Splits])): Map[UniqueArrival, Set[Splits]] = splits + tuple
 }
