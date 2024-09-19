a5a43286cd59d91f7e8de38261f2f41c1bcce02a
Rich Birch <ringo@albumsnaps.com>
2024-09-19 13:08:28 +0100

    DRTII-1593 Update ConfigActor to produce TerminalUpdateRequests

--- jvm/src/test/scala/uk/gov/homeoffice/drt/actor/ConfigActorSpec.scala
+++ jvm/src/test/scala/uk/gov/homeoffice/drt/actor/ConfigActorSpec.scala
@@ -6,8 +6,9 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 import uk.gov.homeoffice.drt.actor.commands.Commands.GetState
-import uk.gov.homeoffice.drt.actor.commands.{Commands, CrunchRequest}
+import uk.gov.homeoffice.drt.actor.commands.{Commands, CrunchRequest, TerminalUpdateRequest}
 import uk.gov.homeoffice.drt.ports.Queues.{EGate, EeaDesk}
+import uk.gov.homeoffice.drt.ports.Terminals.T1
 import uk.gov.homeoffice.drt.ports.config.slas.{SlaConfigs, SlasUpdate}
 import uk.gov.homeoffice.drt.time.TimeZoneHelper.europeLondonTimeZone
 import uk.gov.homeoffice.drt.time.{LocalDate, SDate, SDateLike}
@@ -26,17 +27,17 @@ class ConfigActorSpec
   }
 
   val myNow: () => SDateLike = () => SDate("2023-10-10T00:00")
-  val crunchRequest: LocalDate => CrunchRequest = date => CrunchRequest(SDate(date).toLocalDate)
+  val updateRequest: LocalDate => Seq[TerminalUpdateRequest] = date => Seq(TerminalUpdateRequest(T1, SDate(date).toLocalDate))
 
   "A new config actor" must {
     "have empty state" in {
-      val actor = system.actorOf(Props(new ConfigActor("test-id", myNow, crunchRequest, 1)))
+      val actor = system.actorOf(Props(new ConfigActor("test-id", myNow, updateRequest, 1)))
       actor ! GetState
       expectMsg(SlaConfigs.empty)
     }
 
     "contain a config after a SetUpdate command, and be empty after a RemoveConfig command" in {
-      val actor = system.actorOf(Props(new ConfigActor("test-id", myNow, crunchRequest, 1)))
+      val actor = system.actorOf(Props(new ConfigActor("test-id", myNow, updateRequest, 1)))
       val update = SlasUpdate(1L, Map(EGate -> 1), Option(2L))
       actor ! ConfigActor.SetUpdate(update)
       actor ! GetState
@@ -48,7 +49,7 @@ class ConfigActorSpec
     }
 
     "replace a config after a SetUpdate command" in {
-      val actor = system.actorOf(Props(new ConfigActor("test-id", myNow, crunchRequest, 1)))
+      val actor = system.actorOf(Props(new ConfigActor("test-id", myNow, updateRequest, 1)))
       val update = SlasUpdate(1L, Map(EGate -> 1), None)
       val update2 = SlasUpdate(2L, Map(EeaDesk -> 10), Option(1L))
       actor ! ConfigActor.SetUpdate(update)
@@ -61,7 +62,7 @@ class ConfigActorSpec
     }
 
     "contain two configs after 2 SetUpdate commands" in {
-      val actor = system.actorOf(Props(new ConfigActor("test-id", myNow, crunchRequest, 1)))
+      val actor = system.actorOf(Props(new ConfigActor("test-id", myNow, updateRequest, 1)))
       val update = SlasUpdate(1L, Map(EGate -> 1), None)
       val update2 = SlasUpdate(2L, Map(EeaDesk -> 10), None)
 
@@ -79,15 +80,15 @@ class ConfigActorSpec
       val yesterday = SDate("2023-10-10T00:00", europeLondonTimeZone)
       val today = SDate("2023-10-11T00:00", europeLondonTimeZone)
 
-      val actor = system.actorOf(Props(new ConfigActor("test-id", () => today, crunchRequest, 1)))
+      val actor = system.actorOf(Props(new ConfigActor("test-id", () => today, updateRequest, 1)))
       val update = SlasUpdate(yesterday.millisSinceEpoch, Map(EGate -> 1), None)
       val testProbe = TestProbe()
 
       actor ! Commands.AddUpdatesSubscriber(testProbe.ref)
       actor ! ConfigActor.SetUpdate(update)
 
-      testProbe.expectMsg(CrunchRequest(LocalDate(2023, 10, 11)))
-      testProbe.expectMsg(CrunchRequest(LocalDate(2023, 10, 12)))
+      testProbe.expectMsg(TerminalUpdateRequest(T1, LocalDate(2023, 10, 11)))
+      testProbe.expectMsg(TerminalUpdateRequest(T1, LocalDate(2023, 10, 12)))
     }
   }
 }
