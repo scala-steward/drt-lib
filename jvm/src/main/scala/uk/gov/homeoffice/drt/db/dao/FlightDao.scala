197ad38652e450d5fe1d39b3a36f1af923d59e36
Rich Birch <ringo@albumsnaps.com>
2024-11-07 09:38:41 +0000

    DRTII-1678 Add removal queries and tests

--- jvm/src/main/scala/uk/gov/homeoffice/drt/db/dao/FlightDao.scala
+++ jvm/src/main/scala/uk/gov/homeoffice/drt/db/dao/FlightDao.scala
@@ -1,55 +1,82 @@
 package uk.gov.homeoffice.drt.db.dao
 
 import slick.dbio.Effect
-import uk.gov.homeoffice.drt.arrivals.ApiFlightWithSplits
+import uk.gov.homeoffice.drt.arrivals.{ApiFlightWithSplits, UniqueArrival}
 import uk.gov.homeoffice.drt.db.Db.slickProfile.api._
 import uk.gov.homeoffice.drt.db.serialisers.FlightSerialiser
 import uk.gov.homeoffice.drt.db.tables.{FlightRow, FlightTable}
 import uk.gov.homeoffice.drt.ports.PortCode
+import uk.gov.homeoffice.drt.ports.Terminals.Terminal
 import uk.gov.homeoffice.drt.time.UtcDate
 
 import java.sql.Timestamp
 import scala.concurrent.ExecutionContext
 
 
-case class FlightDao(portCode: PortCode)
+case class FlightDao()
                     (implicit ec: ExecutionContext) {
   val table: TableQuery[FlightTable] = TableQuery[FlightTable]
 
-  val toRow: ApiFlightWithSplits => FlightRow = FlightSerialiser.toRow(portCode)
+  def get(port: PortCode): (PortCode, Terminal, Long, Int) => DBIOAction[Option[ApiFlightWithSplits], NoStream, Effect.Read] =
+    (origin, terminal, scheduled, voyageNumber) =>
+      table
+        .filter(f =>
+          f.origin === origin.iata &&
+            f.port === port.iata &&
+            f.terminal === terminal.toString &&
+            f.scheduled === new Timestamp(scheduled) &&
+            f.voyageNumber === voyageNumber)
+        .result
+        .map(_.map(FlightSerialiser.fromRow).headOption)
 
-  def get(port: String, origin: String, terminal: String, scheduled: Long, voyageNumber: Int): DBIOAction[Seq[ApiFlightWithSplits], NoStream, Effect.Read] =
-    table
-      .filter(f =>
-        f.origin === origin &&
+  def getForTerminalDate(port: String): (String, UtcDate) => DBIOAction[Seq[ApiFlightWithSplits], NoStream, Effect.Read] =
+    (terminal, date) =>
+      table
+        .filter(f =>
           f.port === port &&
-          f.terminal === terminal &&
-          f.scheduled === new Timestamp(scheduled) &&
-          f.voyageNumber === voyageNumber)
-      .result
-      .map(_.map(FlightSerialiser.fromRow))
+            f.terminal === terminal &&
+            f.scheduledDateUtc === date.toISOString
+        )
+        .result
+        .map(_.map(FlightSerialiser.fromRow))
 
-  def getForDatePortTerminalDate(port: String, terminal: String, date: UtcDate): DBIOAction[Seq[ApiFlightWithSplits], NoStream, Effect.Read] =
+  def getForDateDate(port: String): UtcDate => DBIOAction[Seq[ApiFlightWithSplits], NoStream, Effect.Read] =
+    date =>
+      table
+        .filter(f => f.port === port && f.scheduledDateUtc === date.toISOString)
+        .result
+        .map(_.map(FlightSerialiser.fromRow))
+
+  def insertOrUpdate(port: PortCode): ApiFlightWithSplits => DBIOAction[Int, NoStream, Effect.Write with Effect.Transactional] = {
+    val toRow: ApiFlightWithSplits => FlightRow = FlightSerialiser.toRow(port)
+    flight =>
+      table.insertOrUpdate(toRow(flight))
+  }
+
+  def insertOrUpdateMulti(port: PortCode): Iterable[ApiFlightWithSplits] => DBIOAction[Int, NoStream, Effect.Write] = {
+    val toRow: ApiFlightWithSplits => FlightRow = FlightSerialiser.toRow(port)
+    flights => {
+      val inserts = flights.map(f => table.insertOrUpdate(toRow(f)))
+      DBIO.sequence(inserts).map(_.sum)
+    }
+  }
+
+  def remove(port: PortCode): UniqueArrival => DBIOAction[Int, NoStream, Effect.Write] =
+    ua => remove(port, ua)
+
+  def removeMulti(port: PortCode): Iterable[UniqueArrival] => DBIOAction[Int, NoStream, Effect.Write] =
+    uas => {
+      val queries = uas.map(ua => remove(port, ua))
+      DBIO.sequence(queries).map(_.sum)
+    }
+
+  private def remove(port: PortCode, ua: UniqueArrival): DBIOAction[Int, NoStream, Effect.Write] =
     table
       .filter(f =>
-        f.port === port &&
-          f.terminal === terminal &&
-          f.scheduledDateUtc === date.toISOString
-      )
-      .result
-      .map(_.map(FlightSerialiser.fromRow))
-
-  def getForDatePortDate(port: String, date: UtcDate): DBIOAction[Seq[ApiFlightWithSplits], NoStream, Effect.Read] =
-    table
-      .filter(f => f.port === port && f.scheduledDateUtc === date.toISOString)
-      .result
-      .map(_.map(FlightSerialiser.fromRow))
-
-  def insertOrUpdate(flight: ApiFlightWithSplits): DBIOAction[Int, NoStream, Effect.Write with Effect.Transactional] =
-    table.insertOrUpdate(toRow(flight))
-
-  def insertOrUpdate(flights: Iterable[ApiFlightWithSplits]): DBIOAction[Int, NoStream, Effect.Write] = {
-    val inserts = flights.map(f => table.insertOrUpdate(toRow(f)))
-    DBIO.sequence(inserts).map(_.sum)
-  }
+        f.port === port.toString &&
+          f.terminal === ua.terminal.toString &&
+          f.origin === ua.origin.iata &&
+          f.scheduled === new Timestamp(ua.scheduled) &&
+          f.voyageNumber === ua.number)
+      .delete
 }
--- jvm/src/test/scala/uk/gov/homeoffice/drt/db/dao/FlightDaoTest.scala
+++ jvm/src/test/scala/uk/gov/homeoffice/drt/db/dao/FlightDaoTest.scala
@@ -4,9 +4,11 @@ import org.scalatest.BeforeAndAfter
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import specs2.arguments.sequential
+import uk.gov.homeoffice.drt.arrivals.UniqueArrival
 import uk.gov.homeoffice.drt.db.TestDatabase
 import uk.gov.homeoffice.drt.db.serialisers.FlightRowHelper.generateFlight
 import uk.gov.homeoffice.drt.ports.PortCode
+import uk.gov.homeoffice.drt.ports.Terminals.T1
 
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -19,7 +21,9 @@ class FlightDaoTest extends AnyWordSpec with Matchers with BeforeAndAfter {
 
   import TestDatabase.profile.api._
 
-  val dao: FlightDao = FlightDao(PortCode("LHR"))
+  private val portCode: PortCode = PortCode("LHR")
+
+  val dao: FlightDao = FlightDao()
 
   println(dao.table.schema.createStatements.mkString("\n"))
 
@@ -35,9 +39,9 @@ class FlightDaoTest extends AnyWordSpec with Matchers with BeforeAndAfter {
     "insert records into an empty table" in {
       val flight = generateFlight(123, 1L, PortCode("JFK"))
 
-      Await.result(db.run(dao.insertOrUpdate(flight)), 2.second)
+      Await.result(db.run(dao.insertOrUpdate(portCode)(flight)), 2.second)
 
-      val rows = Await.result(db.run(dao.get("LHR", "JFK", "T1", 1L, 123)), 1.second)
+      val rows = Await.result(db.run(dao.get(portCode)(PortCode("JFK"), T1, 1L, 123)), 1.second)
       rows should be(Seq(flight))
     }
 
@@ -45,11 +49,57 @@ class FlightDaoTest extends AnyWordSpec with Matchers with BeforeAndAfter {
       val flight = generateFlight(123, 1L, PortCode("JFK"))
       val flight2 = generateFlight(123, 1L, PortCode("JFK")).copy(apiFlight = flight.apiFlight.copy(Stand = Option("updated stand")))
 
-      Await.result(db.run(dao.insertOrUpdate(flight)), 2.second)
-      Await.result(db.run(dao.insertOrUpdate(flight2)), 2.second)
+      Await.result(db.run(dao.insertOrUpdate(portCode)(flight)), 2.second)
+      Await.result(db.run(dao.insertOrUpdate(portCode)(flight2)), 2.second)
 
-      val rows = Await.result(db.run(dao.get("LHR", "JFK", "T1", 1L, 123)), 1.second)
+      val rows = Await.result(db.run(dao.get(portCode)(PortCode("JFK"), T1, 1L, 123)), 1.second)
       rows should be(Seq(flight2))
+    }
+  }
+
+  "remove" should {
+    "remove records from the table" in {
+      val flight = generateFlight(123, 1L, PortCode("JFK"))
+
+      Await.result(db.run(dao.insertOrUpdate(portCode)(flight)), 2.second)
+      Await.result(db.run(dao.remove(portCode)(UniqueArrival(123, T1, 1L, PortCode("JFK")))), 2.second)
+
+      val rows = Await.result(db.run(dao.get(portCode)(PortCode("JFK"), T1, 1L, 123)), 1.second)
+      rows should be(Seq())
+    }
+  }
+
+  "removeMulti" should {
+    "remove multiple records from the table" in {
+      val flight = generateFlight(123, 1L, PortCode("JFK"))
+      val flight2 = generateFlight(124, 1L, PortCode("JFK"))
+
+      Await.result(db.run(dao.insertOrUpdate(portCode)(flight)), 2.second)
+      Await.result(db.run(dao.insertOrUpdate(portCode)(flight2)), 2.second)
+      Await.result(
+        db.run(dao.removeMulti(portCode)(Seq(
+          UniqueArrival(123, T1, 1L, PortCode("JFK")),
+          UniqueArrival(124, T1, 1L, PortCode("JFK"))))),
+        2.second)
+
+      val rows = Await.result(db.run(dao.get(portCode)(PortCode("JFK"), T1, 1L, 123)), 1.second)
+      rows should be(None)
+    }
+
+    "remove only the records requested and leave other records alone" in {
+      val flight = generateFlight(123, 1L, PortCode("JFK"))
+      val flight2 = generateFlight(124, 1L, PortCode("JFK"))
+
+      Await.result(db.run(dao.insertOrUpdate(portCode)(flight)), 2.second)
+      Await.result(db.run(dao.insertOrUpdate(portCode)(flight2)), 2.second)
+      Await.result(
+        db.run(dao.removeMulti(portCode)(Seq(UniqueArrival(123, T1, 1L, PortCode("JFK"))))),
+        2.second)
+
+      val rows = Await.result(db.run(dao.get(portCode)(PortCode("JFK"), T1, 1L, 123)), 1.second)
+      rows should be(None)
+      val rows2 = Await.result(db.run(dao.get(portCode)(PortCode("JFK"), T1, 1L, 124)), 1.second)
+      rows2 should be(Option(flight2))
     }
   }
 }
