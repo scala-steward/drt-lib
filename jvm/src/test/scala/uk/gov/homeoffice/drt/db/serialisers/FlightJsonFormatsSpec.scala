bba5ec3ba97ba26a07349e354b6874a53ef689c0
Rich Birch <ringo@albumsnaps.com>
2025-05-14 09:34:18 +0100

    DRTII-1854 Add manual serialiser for Predictions. Update LGW min egates

---
+++ jvm/src/test/scala/uk/gov/homeoffice/drt/db/serialisers/FlightJsonFormatsSpec.scala
@@ -0,0 +1,21 @@
+package uk.gov.homeoffice.drt.db.serialisers
+
+import spray.json._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import uk.gov.homeoffice.drt.arrivals.Predictions
+
+class FlightJsonFormatsSpec extends AnyWordSpec with Matchers with FlightJsonFormats {
+  "PredictionsJsonFormat" should {
+    "deserialise the old field name lastChecked" in {
+      val json = """{"lastChecked":1744732043025,"predictions":{"off-schedule":-10,"walk-time":125,"to-chox":7}}"""
+      val expected = Predictions(1744732043025L, Map("off-schedule" -> -10, "walk-time" -> 125, "to-chox" -> 7))
+      json.parseJson.convertTo[Predictions] shouldEqual expected
+    }
+    "deserialise the new field name lastUpdated" in {
+      val json = """{"lastUpdated":1747211437085,"predictions":{"walk-time":563,"to-chox":8}}"""
+      val expected = Predictions(1747211437085L, Map("walk-time" -> 563, "to-chox" -> 8))
+      json.parseJson.convertTo[Predictions] shouldEqual expected
+    }
+  }
+}
