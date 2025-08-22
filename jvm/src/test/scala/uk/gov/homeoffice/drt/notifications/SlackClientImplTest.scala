b2f350f663ba631f3f651de841b31563ba01dda8
Rich Birch <ringo@albumsnaps.com>
2025-08-22 11:50:50 +0100

    DRTII-1950 Tidying

--- jvm/src/test/scala/uk/gov/homeoffice/drt/notifications/SlackClientImplTest.scala
+++ jvm/src/test/scala/uk/gov/homeoffice/drt/notifications/SlackClientImplTest.scala
@@ -1,6 +1,8 @@
 package uk.gov.homeoffice.drt.notifications
 
 import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.http.scaladsl.model.HttpResponse
+import org.apache.pekko.stream.Materializer
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import uk.gov.homeoffice.drt.HttpClient
@@ -10,22 +12,24 @@ import scala.concurrent.Future
 
 class SlackClientImplTest extends AnyWordSpec with Matchers {
   "SlackClientImpl" should {
+    val url = "http://example.com/webhook"
+
     "be instantiable" in {
-      val slackClient = SlackClientImpl(null, "http://example.com/webhook")
-      slackClient.webhookUrl shouldBe "http://example.com/webhook"
+      val slackClient = SlackClientImpl(null, url)
+      slackClient.webhookUrl shouldBe url
     }
     "send a message via a mock httpclient" in {
-      implicit val system: ActorSystem = org.apache.pekko.actor.ActorSystem()
-      implicit val mat: org.apache.pekko.stream.Materializer = org.apache.pekko.stream.Materializer.matFromSystem(org.apache.pekko.actor.ActorSystem())
+      implicit val system: ActorSystem = ActorSystem()
+      implicit val mat: Materializer = Materializer.matFromSystem(ActorSystem())
       val probe = org.apache.pekko.testkit.TestProbe()
 
       val mockHttpClient = new HttpClient {
-        override def send(httpRequest: org.apache.pekko.http.scaladsl.model.HttpRequest): scala.concurrent.Future[org.apache.pekko.http.scaladsl.model.HttpResponse] = {
+        override def send(httpRequest: org.apache.pekko.http.scaladsl.model.HttpRequest): Future[HttpResponse] = {
           httpRequest.entity.dataBytes.runFold("")(_ + _.utf8String).map(body => probe.ref ! body)
           Future.successful(org.apache.pekko.http.scaladsl.model.HttpResponse())
         }
       }
-      val slackClient = SlackClientImpl(mockHttpClient, "http://example.com/webhook")
+      val slackClient = SlackClientImpl(mockHttpClient, url)
       slackClient.notify("Test message")
       val messageSent = probe.expectMsgType[String]
       messageSent should include("Test message")
